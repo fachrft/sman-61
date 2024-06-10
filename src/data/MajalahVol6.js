0b0adb760f774c2952ede0a6ce8129fda8a24482
Fachrifat <fachrifat05@gmail.com>
2024-06-10 19:42:57 +0700

    nambahin foto

--- src/data/MajalahVol6.js
+++ src/data/MajalahVol6.js
@@ -1,10 +1,194 @@
 export const MajalahVol6 = [
     {
         id: 1,
-        foto: "/majalah/nasa-magz-vol-7.png",
+        foto: "/majalah/nasa-magz-vol-6.png",
     },
     {
         id: 2,
-        foto: "/majalah/nasa-magz-vol-6.png",
+        foto: "/nasamek/d (2).png",
+    },
+    {
+        id: 3,
+        foto: "/nasamek/d (3).png",
+    },
+    {
+        id: 4,
+        foto: "/nasamek/d (4).png",
+    },
+    {
+        id: 5,
+        foto: "/nasamek/d (4).png",
+    },
+    {
+        id: 6,
+        foto: "/nasamek/d (5).png",
+    },
+    {
+        id: 7,
+        foto: "/nasamek/d (6).png",
+    },
+    {
+        id: 8,
+        foto: "/nasamek/d (7).png",
+    },
+    {
+        id: 9,
+        foto: "/nasamek/d (8).png",
+    },
+    {
+        id: 10,
+        foto: "/nasamek/d (9).png",
+    },
+    {
+        id: 11,
+        foto: "/nasamek/d (10).png",
+    },
+    {
+        id: 12,
+        foto: "/nasamek/d (11).png",
+    },
+    {
+        id: 13,
+        foto: "/nasamek/d (12).png",
+    },
+    {
+        id: 14,
+        foto: "/nasamek/d (13).png",
+    },
+    {
+        id: 15,
+        foto: "/nasamek/d (14).png",
+    },
+    {
+        id: 16,
+        foto: "/nasamek/d (15).png",
+    },
+    {
+        id: 17,
+        foto: "/nasamek/d (16).png",
+    },
+    {
+        id: 18,
+        foto: "/nasamek/d (17).png",
+    },
+    {
+        id: 19,
+        foto: "/nasamek/d (18).png",
+    },
+    {
+        id: 20,
+        foto: "/nasamek/d (19).png",
+    },
+    {
+        id: 21,
+        foto: "/nasamek/d (20).png",
+    },
+    {
+        id: 22,
+        foto: "/nasamek/d (21).png",
+    },
+    {
+        id: 23,
+        foto: "/nasamek/d (22).png",
+    },
+    {
+        id: 24,
+        foto: "/nasamek/d (23).png",
+    },
+    {
+        id: 25,
+        foto: "/nasamek/d (24).png",
+    },
+    {
+        id: 26,
+        foto: "/nasamek/d (25).png",
+    },
+    {
+        id: 27,
+        foto: "/nasamek/d (26).png",
+    },
+    {
+        id: 28,
+        foto: "/nasamek/d (27).png",
+    },
+    {
+        id: 29,
+        foto: "/nasamek/d (28)).png",
+    },
+    {
+        id: 30,
+        foto: "/nasamek/d (29).png",
+    },
+    {
+        id: 31,
+        foto: "/nasamek/d (30).png",
+    },
+    {
+        id: 32,
+        foto: "/nasamek/d (31).png",
+    },
+    {
+        id: 33,
+        foto: "/nasamek/d (32).png",
+    },
+    {
+        id: 34,
+        foto: "/nasamek/d (33).png",
+    },
+    {
+        id: 35,
+        foto: "/nasamek/d (34).png",
+    },
+    {
+        id: 36,
+        foto: "/nasamek/d (35).png",
+    },
+    {
+        id: 37,
+        foto: "/nasamek/d (36).png",
+    },
+    {
+        id: 38,
+        foto: "/nasamek/d (37).png",
+    },
+    {
+        id: 39,
+        foto: "/nasamek/d (38).png",
+    },
+    {
+        id: 40,
+        foto: "/nasamek/d (39)).png",
+    },
+    {
+        id: 41,
+        foto: "/nasamek/d (40).png",
+    },
+    {
+        id: 42,
+        foto: "/nasamek/d (41).png",
+    },
+    {
+        id: 43,
+        foto: "/nasamek/d (42).png",
+    },
+    {
+        id: 44,
+        foto: "/nasamek/d (43).png",
+    },
+    {
+        id: 45,
+        foto: "/nasamek/d (44).png",
+    },
+    {
+        id: 46,
+        foto: "/nasamek/d (45).png",
+    },
+    {
+        id: 47,
+        foto: "/nasamek/d (46).png",
+    },
+    {
+        id: 48,
+        foto: "/nasamek/d (47).png",
     },
 ]
